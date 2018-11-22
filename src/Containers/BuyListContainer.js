d7dc4c6287518f42e4c4e2601435057584cd0ecc
Sanchez <sebastian.sanchezschemberger@atos.net>
2018-11-22 16:00:22 -0300

    Some little modifications to show "some" in these containers

---
+++ src/Containers/BuyListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BuyListContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>BuyListContainer</h1>
+            </div>
+        );
+    }
+}
+
+export default BuyListContainer;
---
+++ src/Containers/MakeListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MakeListContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>MakeListContainer</h1>
+            </div>
+        );
+    }
+}
+
+export default MakeListContainer;
