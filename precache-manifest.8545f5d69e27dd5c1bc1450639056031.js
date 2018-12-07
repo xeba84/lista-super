272cf70a6e29419f5650588b495203b835e30715
Sanchez <sebastian.sanchezschemberger@atos.net>
2018-12-07 15:56:50 -0300

    Updates

---
+++ precache-manifest.8545f5d69e27dd5c1bc1450639056031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52ff22357113a8b67ed1",
+    "url": "/yourreponame/static/js/runtime~main.52ff2235.js"
+  },
+  {
+    "revision": "2f22cafc5ce83bb0b20f",
+    "url": "/yourreponame/static/js/main.2f22cafc.chunk.js"
+  },
+  {
+    "revision": "3042a82ec7635aa07461",
+    "url": "/yourreponame/static/js/1.3042a82e.chunk.js"
+  },
+  {
+    "revision": "2f22cafc5ce83bb0b20f",
+    "url": "/yourreponame/static/css/main.3e024a2c.chunk.css"
+  },
+  {
+    "revision": "8a815d96da469b3e99d96fed81fb3f0c",
+    "url": "/yourreponame/index.html"
+  }
+];
